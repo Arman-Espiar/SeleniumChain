57d2208d61530cea9707cbd203f5fa380bf7e9a6
OneDeveloper <OneDeveloper@outlook.com>
2022-08-19 15:35:47 +0430

    Change builder name to config

--- sample/SeleniumChains.Sample/Program.cs
+++ sample/SeleniumChains.Sample/Program.cs
@@ -10,26 +10,26 @@ string webUrl = "https://www.wikipedia.org/";
 new SeleniumChain(driver)
 	.ImplicitWaitingForEachPageToLoad(TimeSpan.FromSeconds(3))
 	.Goto(webUrl)
-	.PageNavigation(builder => builder.ScrollDown())
-	.ClickOnElement(builder => builder
+	.PageNavigation(config => config.ScrollDown())
+	.ClickOnElement(config => config
 		.ClickOnElementById("searchLanguage")
 		.ClickOnElementByCssSelector("#searchLanguage > option:nth-child(21)")
 	)
-	.WriteInElement(builder => builder
+	.WriteInElement(config => config
 		.WriteInElementById("searchInput", "کوروش")
 		.WriteAppendInElementById(" بزرگ")
 		.WritePrependInElementById("پادشاه " + Keys.Enter))
-	.ClickOnElement(builder => builder
+	.ClickOnElement(config => config
 		.ClickOnElementByLinkText("کوروش بزرگ"))
-	.ClickOnElement(builder => builder
+	.ClickOnElement(config => config
 		.ClickOnElementById("p-lang-btn")
 		.ClickOnElementByXPath("/html/body/div[1]/div[2]/div[1]/div/ul[2]/li[3]/a")
 	)
-	.PageNavigation(builder => builder.ScrollDown())
-	.ClickOnElement(builder => builder
+	.PageNavigation(config => config.ScrollDown())
+	.ClickOnElement(config => config
 		.ClickOnElementByCssSelector("#mw-normal-catlinks > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)")
 	)
-	.WriteInElement(builder => builder
+	.WriteInElement(config => config
 		.WriteInElementById("searchInput", "ljklkjfdfdjkfasdfl")
 		.ClearTextInputElementById()
 		.WriteInElementById("Order of Assassins" + Keys.Enter)
