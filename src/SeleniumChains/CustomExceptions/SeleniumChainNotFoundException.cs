b08170424a7e14daf95e73c079a22411b0b51c25
Arman <OneDeveloper@outlook.com>
2023-02-21 10:05:34 +0330

    change to dotnet 7 from dont 6 and upgrade selenium and driver to the last version.

--- src/SeleniumChains/CustomExceptions/SeleniumChainNotFoundException.cs
+++ src/SeleniumChains/CustomExceptions/SeleniumChainNotFoundException.cs
@@ -1,13 +1,17 @@
 ﻿namespace SeleniumChain.CustomExceptions
 {
-	public class SeleniumChainNotFoundException:Exception
-    {
+	public class SeleniumChainNotFoundException : Exception
+	{
 		public SeleniumChainNotFoundException()
 		{
 		}
 
-		public SeleniumChainNotFoundException(string message= $"{nameof(ChainSelenium)} must be set from SetSeleniumChain")
+		public SeleniumChainNotFoundException(string message = $"{nameof(ChainSelenium)} must be set from SetSeleniumChain")
 			: base(message)
+		{
+		}
+
+		public SeleniumChainNotFoundException(string? message, Exception? innerException) : base(message, innerException)
 		{
 		}
 	}
